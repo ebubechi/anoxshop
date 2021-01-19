c1a74e12b0e8f05999db0ea602c6c8f57e923179
ebubechi <emmanuelsonzico@gmail.com>
2021-01-19 05:47:44 +0100

    some few touches

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -18,10 +18,10 @@ const Header = () => {
 
   return (
     <header>
-      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+      <Navbar bg='info' variant='primary' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand>ProShop</Navbar.Brand>
+            <Navbar.Brand>AnoxMerce</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
